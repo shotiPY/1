82ab4b6267dc68fb9022120231fe958f8e1df293
shotyPY <shotagvalia7@gmail.com>
2024-04-18 00:41:53 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,65 @@
+from turtle import *
+
+width (7)
+
+color ("red")
+
+forward(200)
+
+left(90)
+
+forward(200)
+
+left(90)
+
+forward(200)
+        
+left (90)
+
+forward (200)
+
+left (90)
+
+forward (50)
+
+left(90)
+
+color ("brown")
+
+forward(70)
+
+right(90)
+
+forward(30)
+
+right(90)
+
+forward(70)
+
+left (90)
+
+color ("red")
+
+forward (120)
+
+left (90)
+
+forward (200)
+
+begin_fill ()
+
+left (45)
+
+forward (142)
+
+left(90)
+
+forward (142)
+
+end_fill ()
+
+
+
+
+
+exitonclick()
