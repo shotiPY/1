0866043d9f8f6a3e84a93f14d1ddccf3e1798e95
shotiPY <167361714+shotiPY@users.noreply.github.com>
2024-05-30 20:48:19 +0400

    Add files via upload

---
+++ ჯგუფი 32/level 3/homework/level3hw.py
@@ -0,0 +1,35 @@
+# step 1 
+
+name="shoti"
+last_name="gvalia"
+age=14
+hobby="programming"
+country="georgia"
+
+print(name, last_name, age, hobby, country)
+
+#step 2- math
+
+print(10+10)
+
+print(50-35)
+
+print(5*5)
+
+print(50//10)
+
+print(10**2)
+
+print(40+23)
+
+print(40//4)
+
+print(19*4)
+
+print(10//5)
+
+print(90+90)
+ 
+
+ #step 3 ვისწავლე სიტყვები.
+
